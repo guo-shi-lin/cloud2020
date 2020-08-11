9905d329db47dca1f16b3959eb109af97f7d0e66
guo-shi-lin <441587605@qq.com>
2020-08-11 14:47:52 +0800

    zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/controller/OrderZkController.java
@@ -0,0 +1,31 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+/**
+ * @author guoshilin
+ */
+@RestController
+@Slf4j
+public class OrderZkController {
+
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/consumer/payment/zk")
+    public String consumerzk() {
+        return restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+    }
+
+}
---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author ：Guo Shi Lin
+ * @date ： 2020/8/11 10:11
+ * @description：主启动类
+ * @modified By：
+ * @version: 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient//该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
